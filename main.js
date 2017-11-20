410586a0acf347af33c0647935ea27fdb5de376e
Marina Timoshenko <nemascha@gmail.com>
2017-11-20 22:32:42 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var addLink = document.getElementById('submit_input');
+	addLink.addEventListener('click', createLink);
+
+function createLink() {
+	var linksQuantity = document.getElementById('quantity_input').value;
+
+}
