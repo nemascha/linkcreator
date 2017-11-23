cfb3b35b34447a34d3d8db5fcdd5b2eb70c50828
Marina Timoshenko <nemascha@gmail.com>
2017-11-23 22:07:38 +0300

    add var new_link new_link_div

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-var addLink = document.getElementById('submit_input');
-	addLink.addEventListener('click', createLink);
+var submitInputListener = document.getElementById('submit_input');
+	submitInputListener.addEventListener('click', createLink);
+
 
 function createLink() {
 	var linksQuantity = document.getElementById('quantity_input').value;
+	var new_link = document.createElement('a');
+		new_link.innerHTML = "новая ссылка";
+		new_link.href = 'http://google.com';
+	var new_link_div = document.createElement('div');
+		new_link_div.id = container;
+		new_link_div.appendChild(new_link);
+	document.body.appendChild(new_link_div);
 
+		
+
+console.log(linksQuantity);	
 }
+
