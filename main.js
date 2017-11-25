d5f4bf1ffd982ad8cedfbcc26d2883615901ce78
Marina Timoshenko <nemascha@gmail.com>
2017-11-25 13:02:10 +0300

    add function createMultiLink

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 var submitInputListener = document.getElementById('submit_input');
-	submitInputListener.addEventListener('click', createLink);
+	submitInputListener.addEventListener('click', createMultiLink);
+
+
+function createMultiLink() {
+	var linksQuantity = document.getElementById('quantity_input').value;
+	for (var i = 1; i <= linksQuantity; i++) {
+		createLink();
+	}
+
+console.log(linksQuantity);	
+
+}
 
 
 function createLink() {
-	var linksQuantity = document.getElementById('quantity_input').value;
 	var new_link = document.createElement('a');
 		new_link.innerHTML = "новая ссылка";
 		new_link.href = 'http://google.com';
@@ -11,9 +21,6 @@ function createLink() {
 		new_link_div.id = container;
 		new_link_div.appendChild(new_link);
 	document.body.appendChild(new_link_div);
-
-		
-
-console.log(linksQuantity);	
 }
+
 
