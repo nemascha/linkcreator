652ab055d38ddb8d47bcfc45478d9f4cf464e5fe
Marina Timoshenko <nemascha@gmail.com>
2017-12-22 16:25:38 +0300

    ref html add alllinksrembutton

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
 var submitInputListener = document.getElementById('submit-input');
 	submitInputListener.addEventListener('click', createNewLinks);
 
-
-	var allLinksRemButton = document.createElement('button');
-		allLinksRemButton.id = 'all-links-rem-btn';
-		allLinksRemButton.classList.add('all-links-rem-button');
-		allLinksRemButton.innerHTML = 'удалить все ссылки';
-		document.body.appendChild(allLinksRemButton);
-		allLinksRemButton.style.display = 'none';
-		
-		allLinksRemButton.addEventListener('click', function () {
+document.addEventListener('DOMContentLoaded', function () {
+			
+	allLinksRemButton = document.querySelector('#all-links-rem-btn');
+	allLinksRemButton.style.display = 'none';
+	allLinksRemButton.addEventListener('click', function () {
 			if (confirm('Вы действительно хотите удалить все ссылки?')) {
 				var allLinksContainer = document.querySelector('#general-container');
 				allLinksContainer.innerHTML = '';
 			} 	
 		});
-
+});
 	
 function createNewLinks() {
 	
@@ -30,20 +26,30 @@ console.log(linksQuantity);
 
 }	
 
+function* generator() {
+	let ident = 1;
+	while(true){
+		yield ident++;
+	}
+}
+
+var gen = generator();
 
 function createLink(i) {
-	var newLink = document.createElement('a');
-		newLink.id = 'link' + gen.next().value;
-		newLink.classList.add('new-link');
-		newLink.innerHTML = 'новая ссылка' + ' ' + gen.next().value;	
-		newLink.href = 'http://google.com';
-
-	var generalContainer = document.querySelector('#general-container');
-
+	let id = gen.next().value;
+	
+	var generalContainer = document.querySelector('#general-container');	
+	
 	var newLinkContainer = document.createElement('div');
 		newLinkContainer.id = 'container';
 		newLinkContainer.classList.add('new-link-container');
 		
+	var newLink = document.createElement('a');
+		newLink.id = 'link' + '-' + id;
+		newLink.classList.add('new-link');
+		newLink.innerHTML = 'новая ссылка' + ' ' + id;	
+		newLink.href = 'http://google.com';
+	
 	var icon = document.createElement('i');
 		icon.classList.add('fa', 'fa-trash-o');
 
@@ -62,20 +68,6 @@ function createLink(i) {
 		remLinkButton.addEventListener('click', function removeLink () {
 			newLinkContainer.remove();
 		});
+
 }
-
-
-document.addEventListener('DOMContentLoaded', function () {
-	container = document.querySelector('#container');
-	button = document.querySelector('#all-links-rem-btn');
-});
-
-function* generator() {
-	var ident = 1;
-	while(true){
-		yield ident++;
-	}
-}
-
-var gen = generator();
 
